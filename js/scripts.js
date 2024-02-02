2ad09593da8f088975f1910dc774ee8370307684
alexmathews89 <alexmathews@live.com>
2024-02-01 21:13:16 -0800

    created jQuery code for adding new item to the list

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+function newItem() {
+  //1 Adding a new item to the list of items:
+  let li = $("<li></li>");
+  let inputValue = $("#input").val();
+  //let text = $(inputValue);
+  li.append(inputValue);
+
+  if (inputValue === "") {
+    alert("You must write something!");
+  } else {
+    $("#list").append(li);
+  }
+}
